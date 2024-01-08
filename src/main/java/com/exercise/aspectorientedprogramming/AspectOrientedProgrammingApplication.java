12c5c5b8a84a460b4254878a8a927a8f48887293
Delvian Kho <delviankho@Nika.local>
2024-01-08 14:03:21 +0700

    feat(aop): add aop architecture

---
+++ src/main/java/com/exercise/aspectorientedprogramming/AspectOrientedProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.aspectorientedprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectOrientedProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspectOrientedProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/aspectorientedprogramming/AspectOrientedProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.aspectorientedprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspectOrientedProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
